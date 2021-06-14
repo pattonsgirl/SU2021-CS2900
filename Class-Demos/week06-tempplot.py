76ef1c7595d11a8f98055595cb5ef1f7c483fe1e
pattonsgirl <kayleigh.duncan@wright.edu>
2021-06-14 13:02:11 -0400

    added exception handling for bad data

--- Class-Demos/week06-tempplot.py
+++ Class-Demos/week06-tempplot.py
@@ -8,20 +8,25 @@ with open("sitka_weather_2018_simple.csv", 'r') as f:
 
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    #print(header_row)
     highs = []
     dates = []
+    lows = []
     for row in reader:
         #print(row)
-        high = int(row[5])
-        #print(type(row[5]))
-        highs.append(high)
-        date = row[2]
-        #dates.append(date)
-        date_to_dt = datetime.strptime(date, '%Y-%m-%d')
-        dates.append(date_to_dt)
+        try:
+            date = row[2]
+            high = int(row[5])
+            low = int(row[6])
+        except:
+            print(f"Missing data for {date}")
+        else:
+            highs.append(high)
+            lows.append(low)
+            date_to_dt = datetime.strptime(date, '%Y-%m-%d')
+            dates.append(date_to_dt)
 
-print(highs)
+#print(highs)
 #print(dates)
 
 plt.style.use('seaborn')
@@ -29,10 +34,19 @@ fig, ax = plt.subplots()
 #ax.plot(highs, c='red')
 #plt.plot(highs)
 ax.plot(dates, highs, c="red")
+ax.plot(dates, lows, c="blue")
+
 ax.set_title("High Temperature to Based on Day", fontsize = 24)
 ax.set_xlabel("Days of the Month", fontsize = 18)
 ax.set_ylabel('High Temps', fontsize = 18)
 ax.tick_params(axis = 'both', which = 'major', labelsize = 16)
+
+ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+
+
 fig.autofmt_xdate()
 
+plt.savefig("high-lows-sitka-2018.png", bbox_inches='tight')
+
 plt.show()
+
